df04598ce1bebad2306c383f4023eeb9fc05531c
장성준 <g6y116@naver.com>
2023-12-04 16:29:56 +0900

    [Bronze V] Title: A/B, Time: 96 ms, Memory: 12384 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.kt
@@ -0,0 +1,14 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.util.*
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+    val st = StringTokenizer(br.readLine())
+    bw.write("${st.nextToken().toDouble() / st.nextToken().toDouble()}")
+    bw.flush()
+    bw.close()
+}
